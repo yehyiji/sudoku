1b2822432b638787b22867a4501f02105d6ba873
yehyiji <yuhsin.x.yeh@gmail.com>
2026-04-27 16:00:04 +0800

    refactor: clean up project directory by removing obsolete scripts and temporary files

--- test_console.js
+++
@@ -1,9 +0,0 @@
-const puppeteer = require('puppeteer');
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  page.on('console', msg => console.log('PAGE LOG:', msg.text()));
-  page.on('pageerror', err => console.log('PAGE ERROR:', err.message));
-  await page.goto('http://localhost:3000', {waitUntil: 'networkidle0'});
-  await browser.close();
-})();
